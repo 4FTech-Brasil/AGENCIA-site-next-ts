e5bfe486517a624f0de7927b41c5bf7a002afcbe
Fellipe Fontanez <fellipefontanez@gmail.com>
2025-11-11 01:31:00 -0300

    pipe

--- next.config.ts
+++ next.config.ts
@@ -58,6 +58,12 @@ const nextConfig: NextConfig = {
     // Ou se estiver em produção:
     // domains: ['seudominio.com'],
   },
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 const withMDX = createMDX({
